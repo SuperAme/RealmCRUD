0c565c6a0032401d7cca6eb7db81de6ef1d55eb3
Américo <americo.meneses.contractor@bbva.com>
2021-02-04 20:36:42 -0600

    creating view

--- REALMCRUD/Controllers/MainViewController.swift
+++ REALMCRUD/Controllers/MainViewController.swift
@@ -9,12 +9,36 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var data = [
+        ["americo","matematicas", "10"],
+        ["karly","pedagogia", "10"],
+        ["bek","IA", "9"]
+    ]
 
     override func viewDidLoad() {
+        tableView.dataSource = self
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+}
 
-
+extension MainViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "infoCell", for: indexPath) as! InformationTableViewCell
+        cell.nameLbl.text = data[indexPath.row][0]
+        cell.subjectLbl.text = data[indexPath.row][1]
+        cell.scoreLbl.text = data[indexPath.row][2]
+        
+        return cell
+    }
+    
+    
 }
 
